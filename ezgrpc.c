68d530b1a7edbaf18b1f76bf64c1d2e37d8cd5eb
Minato Nakamura Yoshie <mnyoshie@gmx.com>
2024-06-05 19:19:36 +0800

    fix Makefile and update copyright

--- ezgrpc.c
+++ ezgrpc.c
@@ -11,7 +11,7 @@
  *
  * MIT license.
  *
- * Copyright (c) 2023-2024 M. N. Yoshie
+ * Copyright (c) 2023-2024 M. N. Yoshie & Al-buharie Amjari
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
